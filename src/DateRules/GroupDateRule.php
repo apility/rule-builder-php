7f4a61a60677ca74f601d2804bfd249889181cfb
Thomas Alrek <thomas@alrek.no>
2021-09-23 20:07:00 +0200

    Implement Traversable interface

--- src/DateRules/GroupDateRule.php
+++ src/DateRules/GroupDateRule.php
@@ -3,8 +3,11 @@
 namespace Netflex\RuleBuilder\DateRules;
 
 use Carbon\Carbon;
+
 use Netflex\RuleBuilder\RuleCollection;
 use Netflex\RuleBuilder\InvalidConfigurationException;
+
+use Netflex\RuleBuilder\Contracts\Traversable;
 
 /**
  * Validates a group of rules against the same date and returns a unified answer
@@ -12,7 +15,7 @@ use Netflex\RuleBuilder\InvalidConfigurationException;
  * $count is required, can be 'all', 'any' or an integer that determines how many rules must pass the check for the
  * group value to be true
  */
-class GroupDateRule extends DateRule
+class GroupDateRule extends DateRule implements Traversable
 {
     /** @var string|int */
     public $count;
@@ -44,15 +47,31 @@ class GroupDateRule extends DateRule
         }
 
         if (is_int($this->count)) {
-            $ch = collect($this->children);
-            $minLevel = min($ch->count(), $this->count);
+            $children = collect($this->children);
+            $minLevel = min($children->count(), $this->count);
 
-            return $ch->filter(function (DateRule $rule) use ($date) {
+            return $children->filter(function (DateRule $rule) use ($date) {
                 return $rule->validate($date);
             })->count() >= $minLevel;
         }
 
         throw new InvalidConfigurationException("[count] is not a valid value on rule with name {$this->name}. Must be 'any', 'all' or int");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function traverse(callable $callback)
+    {
+        foreach ($this->children as $child) {
+            if ($child instanceof Traversable) {
+                /** @var Traversable $child */
+                $child->traverse($callback);
+                return;
+            }
+
+            $callback($child);
+        }
     }
 
     /**
--- src/DateRules/NotDateRule.php
+++ src/DateRules/NotDateRule.php
@@ -4,7 +4,9 @@ namespace Netflex\RuleBuilder\DateRules;
 
 use Carbon\Carbon;
 
-class NotDateRule extends DateRule
+use Netflex\RuleBuilder\Contracts\Traversable;
+
+class NotDateRule extends DateRule implements Traversable
 {
     /** @var DateRule */
     public ?DateRule $child;
@@ -18,5 +20,22 @@ class NotDateRule extends DateRule
     public function validate(Carbon $date): bool
     {
         return !$this->child->validate($date);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function traverse(callable $callback)
+    {
+        if ($this->child) {
+            if ($this->child instanceof Traversable) {
+                /** @var Traversable $child */
+                $child = $this->child;
+                $child->traverse($callback);
+                return;
+            }
+
+            $callback($this->child);
+        }
     }
 }
