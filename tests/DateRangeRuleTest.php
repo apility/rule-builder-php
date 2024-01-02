e9e0ada0c0d7c845837f89f6e5ebafa3c4bf0682
Tommy Stigen Olsen <tommysolsen@gmail.com>
2024-01-02 11:50:17 +0100

    Test specific case, helping joachim

--- tests/DateRangeRuleTest.php
+++ tests/DateRangeRuleTest.php
@@ -46,6 +46,25 @@ class DateRangeRuleTest extends TestCase
         $this->assertFalse($rule->validate($from->copy()->subMillennia()), 'Any date before to fails');
     }
 
+
+    public function testNonInclusiveness()
+    {
+
+        $week2 = new DateRangeRule(["id" => "2622b2d4-d4ef-4c7b-baf0-787b50481475", "type" => "dateRange", "name" => "Uke 2", "from" => "2024-01-08", "to" => "2024-01-15"], []);
+        $week3 = new DateRangeRule(["id" => "2622b2d4-d4ef-4c7b-baf0-787b50481475", "type" => "dateRange", "name" => "Uke 3", "from" => "2024-01-15", "to" => "2024-01-22"], []);
+
+        $date = Carbon::parse('2024-01-15');
+        $date2 = Carbon::parse('2024-01-15');
+
+        $this->assertFalse($week2->validate($date));
+        $this->assertTrue($week3->validate($date));
+
+        $this->assertSame(
+            $date->toIso8601ZuluString(),
+            $date2->toIso8601ZuluString(),
+        );
+    }
+
     private function _bootstrapRule(?Carbon $from, ?Carbon $to): DateRangeRule
     {
         return new DateRangeRule(['name' => 'yay', 'from' => $from ? $from->toIso8601String() : null, 'to' => $to ? $to->toIso8601String() : null], []);
